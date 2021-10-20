7d94e84e9bae6cace7998041a918b2564db10fdc
Mohammad Hossein Ataie <48953110+Mohammad-Hossein-Ataie@users.noreply.github.com>
2021-10-20 12:30:16 +0330

    Add files via upload

--- square root/square root.py
+++ square root/square root.py
@@ -1,1 +1,13 @@
+# Ali ESmaeilzadeh
+# jadi python course
+import math
+n = int(input())
+s_r = []
 
+for number in range(n):
+    num = float(input())
+    if num > 0:  # to avoid any error when user input a negative value
+        s_r += [("{:.4f}".format(math.sqrt(num)))]
+
+for i in s_r:
+    print(i)
